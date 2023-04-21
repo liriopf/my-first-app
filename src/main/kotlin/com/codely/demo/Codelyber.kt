1e7c19b7d8463cc04fbc5c9067605f34cd9a4fc5
marialip <mlpardo@oesia.com>
2023-04-21 18:29:04 +0200

    pruebas con input que pueden ser null

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -1,9 +1,23 @@
 package com.codely.demo
 
-class Codelyber {
-    fun greet() = "Welcome to kotlin skeleton!!!"
-}
+import java.time.LocalDate
 
 fun main() {
-    println(Codelyber().greet())
+    println("Indroduce una fecha con formato <yyyy-MM-dd>")
+//    val readLine: String? = readLine()
+    val readLine: String? = null
+    //val input = LocalDate.parse(readLine?:"1947-08-14") //Si llega null coge fecha por defecto
+//    val input = LocalDate.parse(readLine!!) //!! se usa para indicar que ese valor nunca va a ser null
+                                              //    , si llegase un null rompe en esta linea
+    //    println("has escrito $input ${input.dayOfWeek}")
+
+    readLine?.let {
+        //Solo entra aqui cuando read line no es null
+        val input = LocalDate.parse(readLine)
+        println("has escrito $input ${input.dayOfWeek}")
+    }
+
+
+
+
 }
