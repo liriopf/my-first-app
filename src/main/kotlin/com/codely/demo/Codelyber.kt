b460bbb12f20957736285256b6976773ea946d26
marialip <mlpardo@oesia.com>
2023-04-21 19:36:19 +0200

    pruebas con input que pueden ser null

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -1,5 +1,7 @@
 package com.codely.demo
 import java.time.LocalDate
+import java.time.Period
+
 fun dateDefaultWhenNull(readLine: String?) {
     val input = LocalDate.parse(readLine ?: "1947-08-14") // Si llega null coge fecha por defecto
     println("Fecha por defecto cuando es null $input ${input.dayOfWeek}")
@@ -15,9 +17,25 @@ fun dateNeverNull(readLine: String?) {
     val input = LocalDate.parse(readLine!!) // No se espera null así que lanza nullPointer
     println("Lanza excepción cuando es null $input ${input.dayOfWeek}")
 }
+
+fun dateNotNullOrEmpty(readLine: String?) {
+    readLine.takeUnless {
+        it.isNullOrEmpty() || it.isNullOrBlank()
+    }?.let { line ->
+        val input = LocalDate.parse(line).also {
+            println("You wrote $it")
+        }
+        with(Period.between(input, LocalDate.now())) {
+            println("The difference between date your wrote and today is ${this.years}")
+        }
+    }
+}
+
 fun main() {
     println("Indroduce una fecha con formato <yyyy-MM-dd>")
     val readLine: String? = readLine()
+
+    dateNotNullOrEmpty(readLine)
 
     dateDefaultWhenNull(readLine)
     dateIgnoreWhenNull(readLine)
